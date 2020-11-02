2d4fcfcfe7f42af3be3d1b2ec747a74ac4ea72dc
Julio Moro Sama <49278008+xSamaa@users.noreply.github.com>
2020-11-02 18:18:19 +0100

    1

--- src/main/java/com/reflex/reflexgame/dao/PersonasDAO.java
+++
@@ -1,7 +0,0 @@
-package com.reflex.reflexgame.dao;
-
-import com.reflex.reflexgame.model.Personas;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonasDAO extends JpaRepository<Personas, Integer > {
-}
